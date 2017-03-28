edb2866bb53a38e211f5d070b19b532dd4a833d3
isosweet <405438526@qq.com>
2017-03-28 09:24:50 +0800

    WishfulCloud-DataServ

---
+++ WishfulCloud-DataServ-Product/src/main/java/com/wishfulcloud/dataServ/product/mapper/FinancingProductMapper.java
@@ -0,0 +1,8 @@
+package com.wishfulcloud.dataServ.product.mapper;
+
+import com.wishfulcloud.commons.persistent.CrudDao;
+import com.wishfulcloud.dataServ.product.entity.FinancingProduct;
+
+public interface FinancingProductMapper extends CrudDao<FinancingProduct>{
+
+}
---
+++ WishfulCloud-DataServ-Product/src/main/java/com/wishfulcloud/dataServ/product/mapper/FinancingProductQuotaMapper.java
@@ -0,0 +1,8 @@
+package com.wishfulcloud.dataServ.product.mapper;
+
+import com.wishfulcloud.commons.persistent.CrudDao;
+import com.wishfulcloud.dataServ.product.entity.FinancingProductQuota;
+
+public interface FinancingProductQuotaMapper extends CrudDao<FinancingProductQuota> {
+
+}
