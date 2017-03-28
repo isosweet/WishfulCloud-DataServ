8e6db6f0e4cd3a4f6499c443ccc93b8b0cadff85
isosweet <405438526@qq.com>
2017-03-28 09:29:43 +0800

    修缮了错误

--- WishfulCloud-DataServ-Product/src/main/java/com/wishfulcloud/dataServ/product/mapper/FinancingProductMapper.java
+++ WishfulCloud-DataServ-Product/src/main/java/com/wishfulcloud/dataServ/product/mapper/FinancingProductMapper.java
@@ -1,8 +1,5 @@
 package com.wishfulcloud.dataServ.product.mapper;
 
-import com.wishfulcloud.commons.persistent.CrudDao;
-import com.wishfulcloud.dataServ.product.entity.FinancingProduct;
-
-public interface FinancingProductMapper extends CrudDao<FinancingProduct>{
+public interface FinancingProductMapper {
 
 }
--- WishfulCloud-DataServ-Product/src/main/java/com/wishfulcloud/dataServ/product/mapper/FinancingProductQuotaMapper.java
+++ WishfulCloud-DataServ-Product/src/main/java/com/wishfulcloud/dataServ/product/mapper/FinancingProductQuotaMapper.java
@@ -1,8 +1,7 @@
 package com.wishfulcloud.dataServ.product.mapper;
 
-import com.wishfulcloud.commons.persistent.CrudDao;
-import com.wishfulcloud.dataServ.product.entity.FinancingProductQuota;
 
-public interface FinancingProductQuotaMapper extends CrudDao<FinancingProductQuota> {
+
+public interface FinancingProductQuotaMapper {
 
 }
