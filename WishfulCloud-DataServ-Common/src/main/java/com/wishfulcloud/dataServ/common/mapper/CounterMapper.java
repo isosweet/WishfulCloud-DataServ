73b54aa8ef18914eb5df829e51212f618dff22ee
isosweet <405438526@qq.com>
2017-04-12 18:15:48 +0800

    计数写了点codeing

---
+++ WishfulCloud-DataServ-Common/src/main/java/com/wishfulcloud/dataServ/common/mapper/CounterMapper.java
@@ -0,0 +1,16 @@
+package com.wishfulcloud.dataServ.common.mapper;
+
+import com.wishfulcloud.commons.persistent.CrudDao;
+import com.wishfulcloud.dataServ.common.entity.Counter;
+
+/**   
+ * @Title: 
+ * @Description: 计数器
+ * @author wangxuezheng
+ * @date 2017年4月12日 下午6:04:21
+ * @version V1.0   
+ *
+ */
+public interface CounterMapper extends CrudDao<Counter>{
+
+}
