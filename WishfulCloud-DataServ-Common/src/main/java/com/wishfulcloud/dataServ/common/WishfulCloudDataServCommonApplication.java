f7972057d5d6922d0d70e217867460c7823902fd
isosweet <405438526@qq.com>
2017-04-12 17:59:07 +0800

    Common数据服务

---
+++ WishfulCloud-DataServ-Common/src/main/java/com/wishfulcloud/dataServ/common/WishfulCloudDataServCommonApplication.java
@@ -0,0 +1,12 @@
+package com.wishfulcloud.dataServ.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishfulCloudDataServCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishfulCloudDataServCommonApplication.class, args);
+	}
+}
